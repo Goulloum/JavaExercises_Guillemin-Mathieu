7fe095c63a4da5c36b34231eb29bc2b031ce4025
goulloum <math.guillemin@hotmail.fr>
2023-10-03 09:24:24 +0200

    exo3

---
+++ Feuille3/exercice3.java
@@ -0,0 +1,17 @@
+package Feuille3;
+
+public class exercice3 {
+    public static void main(String[] args) {
+        Integer count = 0;
+        Integer start = 1;
+        Integer end = 50;
+
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                count += i;
+            }
+        }
+
+        System.out.println("La somme des nombres pairs entre " + start + " et " + end + " est: " + count);
+    }
+}
