596fa8063f67f2adc53b70b4f40c3c9953784533
Goulloum <math.guillemin@hotmail.com>
2023-10-05 11:09:52 +0200

    exo 2

--- Feuille5/exercice2.java
+++ Feuille5/exercice2.java
@@ -11,7 +11,7 @@ public class exercice2 {
 
         for (int i = 0; i < list.size() - 1; i++) {
             for (int j = 0; j < list.size() - i - 1; j++) {
-                if (list.get(j) > list.get(j + 1)) {
+                if (list.get(j) < list.get(j + 1)) {
                     int temp = list.get(j + 1);
                     list.set(j + 1, list.get(j));
                     list.set(j, temp);
